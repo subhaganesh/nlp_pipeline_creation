31f91cc08eb77bd8a97fd61a2f9b77bc0ff0f394
subhaganesh <subhaganesh5@outlook.com>
2024-04-11 22:11:09 +0530

    'requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hatespeechclassification",
+    version="0.0.1",
+    author="sugan",
+    author_email="subhaganesh5@outlook.com",
+    packages=find_packages(),
+    install_requires=[],
+)
